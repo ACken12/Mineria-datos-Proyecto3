fe72165444eddbc16e590a86b9eb26863273f881
ACken <kendallangulo2@gmail.com>
2024-11-19 18:02:14 -0600

    I fixed all files

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from visualizations.analysis import perform_analysis
 
 def main():
     url = "https://github.com/ACken12/Mineria-datos-Proyecto3/blob/main/csv/datos.csv"
-    df_limpio, df_original, encoders = load_and_clean_dataset(url)
+    df_limpio = load_and_clean_dataset(url)
     
     if df_limpio is not None:
         # Mostrar algunos datos de muestra del resultado
